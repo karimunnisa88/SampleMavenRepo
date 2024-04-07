f46a102c204e956ff8e380187d71914d48ec0f2a
karimunnisa88 <smdarshkarim@yahoo.com>
2024-04-07 12:54:08 +0530

    First commit

---
+++ MavenSampleDemo/src/test/java/login/LoginDemo.java
@@ -0,0 +1,45 @@
+package login;
+
+import java.awt.AWTException;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginDemo {
+
+	
+	WebDriver driver;
+	@BeforeTest
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));				
+	}
+	@Test
+	public void Operation() throws InterruptedException, AWTException, IOException
+	{
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(3000);		
+	}
+	
+	@AfterTest
+	public void tearDown() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		driver.close();
+	}
+	
+}
